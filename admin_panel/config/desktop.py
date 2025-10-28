1c0e4add15fa21a8bc413d139140a2e939c1d867
Nodirbek <57404324+Nodirbek75@users.noreply.github.com>
2025-10-28 15:44:09 +0500

    Create desktop.py

---
+++ admin_panel/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Admin Panel",
+            "color": "green",
+            "icon": "octicon octicon-tools",
+            "type": "module",
+            "label": _("Admin Panel")
+        }
+    ]
